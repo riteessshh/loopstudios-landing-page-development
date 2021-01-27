214233e7c498d24f2dc30ca352741363b04926c6
Ritesh Jangra <riteshk2874@gmail.com>
2021-01-27 16:56:34 +0530

    edit5.0
    adding menu button

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 
 $("img.menu").click(function(){
-     $("div.content").toggle("show");
      $("div.items").toggle("show");
+     $("div.closing").toggle("show");
 });
-
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+$("img.closing").click(function(){
+  $("div.items").toggle("show");
+})
