e0f0b35dbe99036c21fc87f1261148ec6b402023
Ritesh Jangra <riteshk2874@gmail.com>
2021-01-27 16:19:41 +0530

    edit5.0

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-$("span").click(function(){
-  $("span").innerHTML("");
+
+$("img.menu").click(function(){
+     $("div.content").toggle("show");
+     $("div.items").toggle("show");
 });
+
+// Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
